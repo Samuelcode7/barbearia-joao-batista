f22e74fc6fbe083a918dbbd792012aa58dcd8140
Samuel Martins <samuellcode7@gmail.com>
2026-04-28 02:12:35 -0300

    Remove initMap function from barbearydoc.js
    Removed the initMap function that initializes the map and marker.

--- projetoIncode-main/barbearydoc.js
+++ projetoIncode-main/barbearydoc.js
@@ -1,19 +1,4 @@
 
-function initMap() {
-   
-    const barbearia = { lat: -6.82646, lng: -35.12433 }; 
-    
-    const map = new google.maps.Map(document.getElementById("map"), {
-        zoom: 17,
-        center: barbearia,
-    });
-
-    const marker = new google.maps.Marker({
-        position: barbearia,
-        map: map,
-        title: 'Barbearia João Batista'
-    });
-}
 
 
 
